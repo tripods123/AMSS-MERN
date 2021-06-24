cb815b124339bda4131d2be075a37914043f9f25
Pujan <pujan007mm@gmail.com>
2021-06-24 23:33:21 +0530

    added gitgnore

---
+++ backend/index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app=express();
+const cors=require('cors');
+const cookieParser=require('cookie-parser');
+const authRouter = require('./routes/authentication_routes');
+
+const bodyParser = require('body-parser');
+const corsOptions = {
+  origin: [
+    "http://localhost:3000",
+    "http://127.0.0.1",
+  ],
+  credentials: true
+}
+
+app.use(bodyParser.json());
+app.use(express.urlencoded({extended:false}));
+app.use(cors(corsOptions));
+app.use(cookieParser());
+app.set('trust proxy', 1);
+
+app.use("/auth",authRouter);
+app.listen(process.env.PORT || 5000, process.env.HOST || '::');
