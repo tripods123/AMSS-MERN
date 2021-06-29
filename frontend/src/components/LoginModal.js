2bdfa0917fff638fec9b79b58cc21b63f3ff4816
drachit631 <drachit631@gmail.com>
2021-06-29 16:13:01 +0530

    login frontend done

--- frontend/src/components/LoginModal.js
+++ frontend/src/components/LoginModal.js
@@ -2,11 +2,18 @@ import React, {useState} from 'react';
 import axios from 'axios';
 import Alert from './Alert';
 import {Link} from 'react-router-dom';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEye } from "@fortawesome/free-solid-svg-icons";
+const eye = <FontAwesomeIcon icon={faEye} />;
 export default function Loginmodal(props){ 
     const [logusername,setlogusername]=useState('');
     const [logpassword,setlogpassword]=useState('');
     const [typeofuser,settypeofuser]=useState('Customer');
     const [loginerror,setloginerror]=useState('');
+    const [passwordShown, setPasswordShown] = useState(false);
+          const togglePasswordVisiblity = () => {
+            setPasswordShown(passwordShown ? false : true);
+          };
     const submitValueLog = (e) => {
         e.preventDefault();
         axios({
@@ -35,14 +42,19 @@ export default function Loginmodal(props){
                 <div class="modal-dialog modal-fullscreen-sm-down">
                     <div class="modal-content">
                         <div class="modal-header">
-                            <h5 class="modal-title" id="staticBackdropLabel">Modal title</h5>
+                            <h5 class="modal-title" id="staticBackdropLabel">Login to</h5>
                             <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                         </div>
                         <div class="modal-body">
-                        <h1 className='display-6'>Login to your account</h1>
+                            <h1 className='display-7'>Automobile Seller</h1>
+                            <br/>
                             <form onSubmit={submitValueLog}>
                                 <input className='form-control' placeholder='Username' id='username' type='text' onChange={e => setlogusername(e.target.value)}/><br/>
-                                <input className='form-control' placeholder='Password' id='password' type='password' onChange={e => setlogpassword(e.target.value)} required/><br/>
+                                <div class="input-group mb-3">
+                                    <input class='w-75 p-3' className='form-control' placeholder='Password' id='password' type={passwordShown ? "text" : "password"} onChange={e => setlogpassword(e.target.value)} required/>
+                                    <i class="btn btn-outline-secondary" onClick={togglePasswordVisiblity}>{eye}</i>  
+                                </div>
+                                <Link  class="float-end" to="/registercustomer" data-bs-dismiss="modal">Forgot Password?</Link>
                                 <br/>
                                 <div className='form-check form-check-inline'>
                                     <input type='radio' className='form-check-input' name='typeofuser' id='customer' value='customer' onChange={e => settypeofuser(e.target.value)}/>
@@ -51,10 +63,15 @@ export default function Loginmodal(props){
                                 <div className='form-check form-check-inline'>
                                     <input type='radio' className='form-check-input' name='typeofuser' value='seller' onChange={e => settypeofuser(e.target.value)}/>
                                     <label htmlFor='seller' className='form-check-label' style={{'color':'#000000'}}>Seller</label>
-                                </div>
+                                </div><br/>
                                 {loginerror === ''?null:<Alert message={loginerror} type='danger'/>}
-                                <Link to="/registercustomer" data-bs-dismiss="modal">New to site?</Link> 
-                                <br/><button tag='input' type='submit' className='btn btn-primary'>Submit</button>
+                                <br/>
+                                <div class="d-inline float-left">
+                                    <button tag='input' type='submit' class='btn btn-primary fs-6 w-auto h-auto'>Login</button>
+                                </div>
+                                <div class="d-inline p-2 float-left">
+                                    <p class='fs-6' >New to site? <Link class='fs-6' to="/registercustomer" data-bs-dismiss="modal">Signup</Link></p>
+                                </div>
                             </form>
                         </div>
                         <div class="modal-footer">
