3b18b444e4a0b3e3658abc6ecf738b6e46b0be2e
Pujan <pujan007mm@gmail.com>
2021-07-20 18:56:40 +0530

    reverting to old commits

--- controllers/usercontroller.js
+++ controllers/usercontroller.js
@@ -2,13 +2,13 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const { MongoClient, ObjectId } = require("mongodb");
-const multer=require('multer');
-const jwt=require('jsonwebtoken');
-const key='09f26e402586e2faa8da4c98a35f1b20d6b033c6097befa8be3486a829587fe2f90a832bd3ff9d42710a4da095a2ce285b009f0c3730cd9b8e1af3eb84df6611';
+const { MongoClient } = require("mongodb");
+const jwt = require('jsonwebtoken');
 const cookieParser = require('cookie-parser');
 const bcrypt = require('bcryptjs');
-const saltRounds = 10;
+// get config consts
+const key='09f26e402586e2faa8da4c98a35f1b20d6b033c6097befa8be3486a829587fe2f90a832bd3ff9d42710a4da095a2ce285b009f0c3730cd9b8e1af3eb84df6611';
+
 
 // access config const
 app.use(cookieParser);
@@ -16,14 +16,14 @@ app.use(cors());
 app.use(bodyParser.json());
 
 const saltRounds = 10;
-export function create (req, res) {
+exports.create=function (req, res) {
     MongoClient.connect(process.env.mongo_url,{ useUnifiedTopology: true }, function (err, client) {
         if (err) throw err
         const db = client.db('amss');
-        genSalt(saltRounds, function(err, salt) {
+        bcrypt.genSalt(saltRounds, function(err, salt) {
             if(err)
                 return res.status(500).send(err);
-            _hash(req.body.password, salt, function(err, hash) { 
+            bcrypt.hash(req.body.password, salt, function(err, hash) { 
                 if(err)
                     return res.status(500).send(err);
                 const tobeinserted={
@@ -39,15 +39,15 @@ export function create (req, res) {
                     const customer = await db.collection('customer').insertOne(tobeinserted);
                     if(customer.insertedCount===1){
                         const payload = {'_id': customer.insertedId,type:'customer','username':req.body.username}
-                        let token = sign(payload, key,{expiresIn: '72h'});
+                        let token = jwt.sign(payload, key,{expiresIn: '72h'});
                         return res.cookie('token', token, {expires: new Date(Date.now() + 72 * 3600000),httpOnly:true,sameSite:'none', secure:true}).send('OK');
                     }
                 })();
             });
         });
     });
-}
-export function available (req, res) {
+};
+exports.available=function (req, res) {
     MongoClient.connect(process.env.mongo_url,{ useUnifiedTopology: true }, function (err, client) {
         if (err) throw err
         const db = client.db("amss");
@@ -61,10 +61,10 @@ export function available (req, res) {
         })();
     });
 }
-export function isallowedgeneral (req, res) {
+exports.isallowedgeneral=function (req, res) {
     const token = req.cookies.token;
     if(token){
-        verify(token, key, (err, decoded) => {
+        jwt.verify(token, key, (err, decoded) => {
             if (err) 
                 return res.status(500).send('Internal Server error');
             if(decoded.type==='customer'){   
@@ -76,12 +76,12 @@ export function isallowedgeneral (req, res) {
     }else{
         return res.status(200).send(true);
     }
-}
+};
 
-export function isallowed (req, res) {
+exports.isallowed=function (req, res) {
     const token = req.cookies.token;
     if(token){
-        verify(token, key, (err, decoded) => {
+        jwt.verify(token, key, (err, decoded) => {
             if (err) 
                 return res.status(500).send('Internal Server error');
             if(decoded.type==='customer'){
@@ -93,4 +93,4 @@ export function isallowed (req, res) {
     }else{
         return res.status(401).send(false);
     }
-}
+};
