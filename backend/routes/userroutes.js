976e4934ca4a373d6cb5de35f76861bbe0d63d10
Pujan <pujan007mm@gmail.com>
2021-08-01 17:56:27 +0530

    fix 5

--- backend/routes/userroutes.js
+++ backend/routes/userroutes.js
@@ -1,6 +1,6 @@
-import { Router } from 'express';
-const router = Router();
-import { create, isallowed, isallowedgeneral, available } from '../controllers/usercontroller';
+const express = require('express');
+const router = express.Router();
+const user=require('../controllers/usercontroller');
 router.post("/create",create);
 router.get("/allowed",isallowed);
 router.get("/allowedgeneral",isallowedgeneral);
