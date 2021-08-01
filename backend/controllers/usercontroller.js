204ee1565acb5aa31b034f6b17124057a405b4dd
Pujan <pujan007mm@gmail.com>
2021-08-01 17:29:29 +0530

    fix fix

--- backend/controllers/usercontroller.js
+++ backend/controllers/usercontroller.js
@@ -1,11 +1,11 @@
-import express from 'express';
+const express = require('express');
 const app = express();
-import { json } from 'body-parser';
-import cors from 'cors';
-import { MongoClient } from "mongodb";
-import { sign, verify } from 'jsonwebtoken';
-import cookieParser from 'cookie-parser';
-import { genSalt, hash as _hash } from 'bcryptjs';
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const { MongoClient } = require("mongodb");
+const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
+const bcrypt = require('bcryptjs');
 // get config consts
 const key='09f26e402586e2faa8da4c98a35f1b20d6b033c6097befa8be3486a829587fe2f90a832bd3ff9d42710a4da095a2ce285b009f0c3730cd9b8e1af3eb84df6611';
 
