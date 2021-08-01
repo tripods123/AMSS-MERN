2ccbde3c59cc18b08ff13dfa324129c854c99ee0
Pujan <pujan007mm@gmail.com>
2021-08-01 17:59:55 +0530

    FINAL FIX

--- backend/routes/userroutes.js
+++ backend/routes/userroutes.js
@@ -5,4 +5,4 @@ router.post("/create",create);
 router.get("/allowed",isallowed);
 router.get("/allowedgeneral",isallowedgeneral);
 router.get("/availability/:username",available);
-export default router;
+module.exports=  router;
