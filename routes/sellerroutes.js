a437b88e71406afa067fbd5c57c5d84cd1704a6e
Pujan <pujan007mm@gmail.com>
2021-08-01 17:56:27 +0530

    fix 5

--- routes/sellerroutes.js
+++ routes/sellerroutes.js
@@ -1,6 +1,6 @@
-import { Router } from 'express';
-const router = Router();
-import { create, isallowed, available, statescities } from '../controllers/sellercontroller';
+const express = require('express');
+const router = express.Router();
+const user=require('../controllers/sellercontroller');
 router.post("/create",create);
 router.get("/allowed",isallowed);
 router.get("/availability/:username",available);
