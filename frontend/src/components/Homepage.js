597fffc93e6016afc741ef962bdddfa75afe8425
Pujan <pujan007mm@gmail.com>
2021-08-07 00:34:49 +0530

    hompeage

--- frontend/src/components/Homepage.js
+++ frontend/src/components/Homepage.js
@@ -5,10 +5,13 @@ function Homepage(){
 		<div>
 			<div className="row justify-content-start">
 				<div className="col-sm">
+					<img src="/img/download.png" width="800px" height="600px"></img>
 				</div>
 				<div className="col-sm">
+					<img src="/img/download.png" width="800px" height="600px"></img>
 				</div>
 				<div className="col-sm">
+					<img src="/img/download.png" width="800px" height="600px"></img>
 				</div>
 			</div>
 		</div>
