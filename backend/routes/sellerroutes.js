571fb9096439aa67d73be2a4a111a0ebc9bd5dda
Kudaligi Amoghavarsha <kgamoghavarsha@gmail.com>
2021-10-05 22:19:41 +0530

    fix bug

--- backend/routes/sellerroutes.js
+++ backend/routes/sellerroutes.js
@@ -4,5 +4,5 @@ const seller=require('../controllers/sellercontroller');
 router.post("/create",seller.create);
 router.get("/allowed",seller.isallowed);
 router.get("/availability/:username",seller.available);
-router.get("/statescities",seller.statescities);
+router.get("/statescities",seller.states);
 module.exports=  router;
