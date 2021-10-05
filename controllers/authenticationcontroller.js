a6e1470417f2164232160abcf24af375af98fa57
Kudaligi Amoghavarsha <kgamoghavarsha@gmail.com>
2021-10-05 22:52:20 +0530

    fix typos

--- controllers/authenticationcontroller.js
+++ controllers/authenticationcontroller.js
@@ -69,7 +69,7 @@ exports.authenticate=function(req, res) {
                         }
                         if(user.length > 0){
                             if(user[0]['isactive']===1){
-                                let token = kwt.sign({_id: user[0]['_id'],type:'seller','username':username}, key,{expiresIn: '72h'});
+                                let token = jwt.sign({_id: user[0]['_id'],type:'seller','username':username}, key,{expiresIn: '72h'});
                                 return res.cookie('token', token, {expires: new Date(Date.now() + 72 * 3600000),httpOnly:true,sameSite:'none', secure:true}).send({   
                                     'links':[{title:'Home', path:'/'},{ title: `About us`, path: `/about` },{ title: `Insert Products`, path: `/insertproduct` },{ title: `All Orders`, path: `/recievedorders` },{ title: `Pending Orders`, path: `/pendingorders` },{ title: `FAQ`, path: `/faq` },{ title: `Logout`, path: `/logout` }],
                                     'userType':'seller'                        
