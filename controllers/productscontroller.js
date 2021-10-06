dd0cd3561462842808579659695f1bcd47db0a12
Kudaligi Amoghavarsha <kgamoghavarsha@gmail.com>
2021-10-06 13:29:58 +0530

    fix urls

--- controllers/productscontroller.js
+++ controllers/productscontroller.js
@@ -9,20 +9,8 @@ const multer=require('multer');
 const firebase = require("firebase/app");
 require('firebase/storage');
 global.XMLHttpRequest=require('xhr2');
-const firebaseConfig = {
-    apiKey: process.env.apiKey,
-    authDomain: process.env.authDomain,
-    projectId: process.env.projectId,
-    storageBucket: process.env.storageBucket,
-    messagingSenderId: process.env.messagingSenderId,
-    appId: process.env.appId,
-    measurementId: process.env.measurementId
-};
-
-firebase.initializeApp(firebaseConfig);
 const upload = multer({storage:multer.memoryStorage()}).array('image');
 const storageRef = firebase.storage().ref();
-
 app.use(cookieParser());
 app.use(cors());
 app.use(bodyParser.json());
